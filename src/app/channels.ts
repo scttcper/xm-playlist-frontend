e4be8b2cbb3a98185aa17c094ff661667e7c5a3d
Scott Cooper <scttcper@gmail.com>
2019-08-05 21:34:02 -0700

    add poprocks

--- src/app/channels.ts
+++ src/app/channels.ts
@@ -112,13 +112,14 @@ export const channels: Channel[] = [
     desc: 'Bright Pop Hits',
     playlist: '4U1w1DNhpN6Y8ZvxvJpwn0',
   },
-  // {
-  //   id: 'siriuslove',
-  //   number: 17,
-  //   name: 'SiriusXM Love',
-  //   genre: 'Pop',
-  //   desc: 'Love Songs',
-  // },
+  {
+    id: '9450',
+    number: 17,
+    name: 'PopRocks',
+    genre: 'Pop',
+    desc: 'Rock & Pop from the \'90s & 2Ks',
+    playlist: '1PcSu1p5PaLOssq9VnW30g',
+  },
   // {
   //   id: '9446',
   //   number: 18,
